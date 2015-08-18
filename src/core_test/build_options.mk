46eb8092b694ac4b4ea8d595e6cee2839e9247fc
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2015-08-18 09:09:38 -0600

    Adding filter support to the test core
    This commit adds support to the test core for filters. Additionally, it
    adds two filters to the test core that test the compute interval of
    filters.

--- src/core_test/build_options.mk
+++ src/core_test/build_options.mk
@@ -3,5 +3,11 @@ EXE_NAME=test_model
 NAMELIST_SUFFIX=test
 override CPPFLAGS += -DCORE_TEST
 
+ifneq ( , $(findstring filters, $(PRE_CORE_TARGETS) ) )
+	override PRE_CORE_TARGETS += filters
+	FCINCLUDES += -I$(PWD)/src/filters
+	LIBS += -lfilters
+endif
+
 report_builds:
 	@echo "CORE=test"
